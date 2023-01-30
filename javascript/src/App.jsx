fe99416d705c10bb87a5165276d65bd1f01d65e7
Nick Mastoris <nmastoris@cti.gr>
2023-01-30 09:42:28 +0200

    remove some imports that causes problems

--- javascript/src/App.jsx
+++ javascript/src/App.jsx
@@ -7,8 +7,8 @@ import {QueryClient, QueryClientProvider} from 'react-query'
 import Communities from "./Pages/Communities";
 import Users from "./Pages/Users";
 import Dashboard from "./Pages/Dashboard";
-import Idps from "./Pages/Idps";
-import Sps from "./Pages/Sps";
+// import Idps from "./Pages/Idps";
+// import Sps from "./Pages/Sps";
 
 function App() {
     // const queryClient = new QueryClient()
