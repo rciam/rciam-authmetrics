55a92b67061e2272cb39c2e09901e0198477cdb7
Nick Mastoris <nmastoris@cti.gr>
2023-01-30 09:36:28 +0200

    add page for Dashboard

---
+++ javascript/src/Pages/Dashboard/index.js
@@ -0,0 +1,43 @@
+import { useState, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from '../../utils/api';
+import Container from "react-bootstrap/Container";
+import LoginDataTable from "../../components/Dashboard/loginDataTable";
+import LoginIdpPieChart from "../../components/Dashboard/loginIdpPieChart";
+import LoginLineChart from "../../components/Dashboard/loginLineChart";
+import LoginsMap from "../../components/Dashboard/loginsMap";
+import LoginSpPieChart from "../../components/Dashboard/loginSpPieChart";
+import LoginTiles from "../../components/Dashboard/loginTiles";
+import IdpModal from "../Idps/idpModal";
+
+const Dashboard = () => {
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const {project, environment } = useParams();
+    const [tenantId, setTenantId] = useState(0);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {   
+        client.get("tenant/" + project + "/" + environment).
+            then(response => {
+                
+                setTenantId(response["data"][0]["id"])
+            })    
+    }, [])
+    if(tenantId == 0)
+        return
+    else return (
+    <Container>
+        <h1>Dashboard</h1>
+        <LoginTiles tenantId={tenantId}></LoginTiles>
+        <LoginLineChart tenantId={tenantId}></LoginLineChart>
+        {/* <LoginIdpPieChart tenantId={tenantId} setShowModalHandler={setShowModal}></LoginIdpPieChart>
+        <LoginSpPieChart tenantId={tenantId} setShowModalHandler={setShowModal}></LoginSpPieChart>
+        <LoginDataTable startDateHandler={setStartDate} endDateHandler={setEndDate} tenantId={tenantId}></LoginDataTable>
+        <LoginsMap startDate={startDate} endDate={endDate} tenantId={tenantId}></LoginsMap> */}
+
+        <IdpModal tenantId={tenantId} showModal={showModal} setShowModalHandler={setShowModal}></IdpModal>
+    </Container>)
+
+}
+export default Dashboard;
