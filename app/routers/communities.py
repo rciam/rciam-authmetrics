2cce5442e5f6ccd52b3b7be422521b7c4bd2e908
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2023-03-22 11:18:22 +0200

    Reorganize routers to files

---
+++ app/routers/communities.py
@@ -0,0 +1,137 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import List, Union
+
+from app.database import get_session
+from app.models.community_info_model import *
+from app.models.community_model import *
+from app.models.member_model import MembersReadWithCommunityInfo
+
+# from ..dependencies import get_token_header
+
+
+MembersReadWithCommunityInfo.update_forward_refs(
+    Community_InfoRead=Community_InfoRead)
+
+router = APIRouter(
+    tags=["communities"],
+    # dependencies=[Depends(get_token_header)],
+    # responses={404: {"description": "Not found"}},
+)
+
+# @router.get("/communities/", response_model=List[CommunityReadwithInfo])
+# def read_communities(
+#     *,
+#     session: Session = Depends(get_session),
+#     offset: int = 0
+# ):
+
+#     communities = session.exec(select(Community).offset(offset)).all()
+#     return communities
+
+@router.get("/members/", response_model=List[MembersReadWithCommunityInfo])
+def read_members(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    # community_id: Union[None, int] = None
+):
+    # if not community_id:
+    #     members = session.exec(select(Members).offset(offset)).all()
+    # else:
+    members = session.exec(select(Members).offset(offset)).all()
+    return members
+
+
+@router.get("/members_bystatus/")
+def read_members_bystatus(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    community_id: Union[None, int] = None,
+    tenant_id: int,
+):
+    if not community_id:
+        members = session.exec(select(Members).offset(offset)).all()
+    else:
+        # members = session.exec(select(Members).offset(offset)).all()
+        members = session.exec("""
+             SELECT count(*) as count, community_id, status FROM members 
+             WHERE community_id={0} AND tenant_id={1}
+             GROUP BY community_id, status
+          """.format(community_id, tenant_id)).all()
+        # members = session.exec(""" SELECT community_id FROM members """)
+    return members
+
+
+@router.get("/communities_groupby/{group_by}")
+def read_communities(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    group_by: str,
+    tenant_id: int,
+    interval: Union[str, None] = None,
+    count_interval: int = None,
+    startDate: str = None,
+    endDate: str = None,
+):
+    interval_subquery = ""
+    if group_by:
+        if interval and count_interval:
+            interval_subquery = """WHERE created >
+        date_trunc('{0}', CURRENT_DATE) - INTERVAL '{1} {2}'""".format(group_by, count_interval, interval)
+        if startDate and endDate:
+            interval_subquery = """
+                WHERE created BETWEEN '{0}' AND '{1}'
+            """.format(startDate, endDate)
+        if interval_subquery == "":
+            interval_subquery = """
+                WHERE community.tenant_id={0}
+            """.format(tenant_id)
+        else:
+            interval_subquery += """ AND community.tenant_id={0} 
+            """.format(tenant_id)
+
+        communities = session.exec("""
+        select count(*) as count, date_trunc( '{0}', created ) as range_date, 
+            min(created) as min_date , string_agg(name,'|| ') as names, 
+            string_agg(to_char(created, 'YYYY-MM-DD'),', ') as created_date, 
+            string_agg(description,'|| ') as description
+        from community
+        join community_info on community.community_id=community_info.id
+        {1}
+        group by range_date
+        ORDER BY range_date ASC
+        """.format(group_by, interval_subquery)).all()
+    return communities
+
+
+@router.get("/communities/")
+def read_community(
+    *,
+    session: Session = Depends(get_session),
+    community_id: Union[None, int] = None,
+        tenant_id: int):
+    sql_subquery = ''
+    if community_id:
+        sql_subquery = 'id={0} and'.format(community_id)
+    community = session.exec("""
+        SELECT * FROM community_info WHERE {0} tenant_id={1}
+    """.format(sql_subquery, tenant_id)).all()
+    # statement = select(Community).options(selectinload(Community.community_info))
+    # result = session.exec(statement)
+    # community = result.one()
+    # if not community:
+    #     raise HTTPException(status_code=404, detail="Community not found")
+    return community
+
+
+@router.get("/communities_info/", response_model=List[Community_InfoRead])
+def read_communities_info(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0
+):
+    communities = session.exec(select(Community_Info).offset(offset)).all()
+    return communities
---
+++ app/routers/users.py
@@ -0,0 +1,162 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Union
+
+from app.database import get_session
+
+# from ..dependencies import get_token_header
+
+router = APIRouter(
+    tags=["users"],
+    # dependencies=[Depends(get_token_header)],
+    # responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/registered_users_country")
+def read_users_country(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    startDate: str = None,
+    endDate: str = None,
+    tenant_id: int
+):
+    interval_subquery = ""
+    if startDate and endDate:
+        interval_subquery = """
+                WHERE users.created BETWEEN '{0}' AND '{1}'
+            """.format(startDate, endDate)
+    users_countries = session.exec(
+        """WITH users_countries AS (
+        SELECT statistics_country_hashed.hasheduserid as userid, country, countrycode, count(*) as sum_count
+        FROM statistics_country_hashed
+        JOIN country_codes ON countryid=country_codes.id
+        WHERE tenant_id = {1}
+        GROUP BY userid, country, countrycode
+        ),
+        max_count_users_countries AS (
+            SELECT DISTINCT userid, max(sum_count) as max_sum_count,row_number() OVER (ORDER BY userid) as row_number 
+            FROM users_countries
+            GROUP BY  userid
+        )
+        SELECT country,countrycode, count(*) as sum 
+            FROM users_countries
+            JOIN (
+                    SELECT userid, max_sum_count, max(row_number) 
+                    FROM max_count_users_countries GROUP BY userid, max_sum_count 
+                ) max_count_users_countries_no_duplicates
+            ON users_countries.userid=max_count_users_countries_no_duplicates.userid 
+                AND users_countries.sum_count=max_count_users_countries_no_duplicates.max_sum_count
+            JOIN users ON users.hasheduserid=users_countries.userid AND status='A'
+            {0}
+            GROUP BY country,countrycode
+            ORDER BY country,countrycode
+        """.format(interval_subquery, tenant_id)).all()
+    return users_countries
+
+
+@router.get("/registered_users_country_group_by/{group_by}")
+def read_users_country_groupby(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    group_by: str,
+    startDate: str = None,
+    endDate: str = None,
+    tenant_id: int
+):
+    if group_by:
+        interval_subquery = ""
+        if startDate and endDate:
+            interval_subquery = """
+                WHERE users.created BETWEEN '{0}' AND '{1}'
+            """.format(startDate, endDate)
+        users = session.exec(
+            """WITH users_countries AS (
+        SELECT statistics_country_hashed.hasheduserid as userid, country, countrycode, count(*) as sum_count
+        FROM statistics_country_hashed
+        JOIN country_codes ON countryid=country_codes.id
+        WHERE tenant_id = {2}
+        GROUP BY userid, country, countrycode
+        ),
+        max_count_users_countries AS (
+            SELECT DISTINCT userid, max(sum_count) as max_sum_count,row_number() OVER (ORDER BY userid) as row_number 
+            FROM users_countries
+            GROUP BY  userid
+        )
+        SELECT range_date, min(created_min_date) as min_date, STRING_AGG(country, '|| ') as countries, sum(sum)  as count
+        FROM 
+            (
+                SELECT date_trunc('{0}', users.created) as range_date, CONCAT(country,': ',count(*)) as country, min(users.created) as created_min_date,  count(*) as sum 
+                FROM users_countries
+                JOIN (
+                        SELECT userid, max_sum_count, max(row_number) 
+                        FROM max_count_users_countries GROUP BY userid, max_sum_count 
+                    ) max_count_users_countries_no_duplicates
+                ON users_countries.userid=max_count_users_countries_no_duplicates.userid 
+                    AND users_countries.sum_count=max_count_users_countries_no_duplicates.max_sum_count
+                JOIN users ON users.hasheduserid=users_countries.userid AND status='A'
+                {1}
+                GROUP BY range_date, country,countrycode
+                ORDER BY range_date, country
+            ) user_country_group_by
+        GROUP BY range_date""".format(group_by, interval_subquery, tenant_id)).all()
+        return users
+
+
+@router.get("/registered_users_groupby/{group_by}")
+def read_users_groupby(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    group_by: str,
+    interval: Union[str, None] = None,
+    count_interval: int = None,
+    startDate: str = None,
+    endDate: str = None,
+    tenant_id: int
+):
+
+    interval_subquery = ""
+    if group_by:
+        if interval and count_interval:
+            interval_subquery = """AND created >
+        date_trunc('{0}', CURRENT_DATE) - INTERVAL '{1} {2}'""".format(group_by, count_interval, interval)
+        if startDate and endDate:
+            interval_subquery = """
+                AND created BETWEEN '{0}' AND '{1}'
+            """.format(startDate, endDate)
+        users = session.exec("""
+        select count(*) as count, date_trunc( '{0}', created ) as range_date, 
+            min(created) as min_date
+        from users
+        WHERE status = 'A' AND tenant_id = {1}
+        {2}
+        group by range_date
+        ORDER BY range_date ASC
+        """.format(group_by, tenant_id, interval_subquery)).all()
+    return users
+
+
+@router.get("/registered_users_countby")
+def read_users_countby(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    interval: Union[str, None] = None,
+    count_interval: int = None,
+    tenant_id: int
+):
+
+    interval_subquery = ""
+    if interval and count_interval:
+        interval_subquery = """AND created >
+        CURRENT_DATE - INTERVAL '{0} {1}'""".format(count_interval, interval)
+
+    users = session.exec("""
+    select count(*) as count
+    from users
+    WHERE status = 'A' AND tenant_id = {1}
+    {0}""".format(interval_subquery, tenant_id)).all()
+    return users
