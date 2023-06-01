72667e7f00886bc412e288539d3b60f5f3bdd3e1
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2023-06-01 14:57:22 +0300

    initial config

---
+++ authorize.py
@@ -0,0 +1,22 @@
+[anonymous]
+dashboard;view
+identity_providers;view
+service_providers;view
+
+[authenticated]
+dashboard;view
+identity_providers;view
+service_providers;view
+
+[administrator]
+dashboard;view,write
+identity_providers;view,write
+service_providers;view,write
+registered_users;view,write
+communities;view,write
+
+%% This is a comment
+[entitlements]
+urn:mace:egi.eu:group:stats-viewers.aai.egi.eu:role=member#aai.egi.eu;administrator
+
+
