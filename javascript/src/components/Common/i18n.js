91b84cd49069159ddbc3d5d337e870e270178c2f
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2023-07-10 09:29:54 +0300

    Set locales path to metrics

--- javascript/src/components/Common/i18n.js
+++ javascript/src/components/Common/i18n.js
@@ -1,5 +1,5 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
@@ -20,6 +20,9 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    backend: {
+      loadPath: 'metrics/locales/{{lng}}/{{ns}}.json'
+    },
     fallbackLng: 'en',
     debug: false,
 
