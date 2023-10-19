bd424258edc955d960b4bc1e5933f5634632cd84
Nick Mastoris <lionick85@hotmail.com>
2023-10-19 09:29:45 +0300

    set pool size and max overflow

--- app/database.py
+++ app/database.py
@@ -5,9 +5,12 @@ from sqlmodel import create_engine, Session
 def get_session():
     # Initialize
     config_file = 'config.global.py'
+    db_params = configParser.getConfig('database_parameters', config_file)
 
-    url = configParser.getConfig('database_parameters', config_file)['database_url']
-    engine = create_engine(url)
+    url = db_params['database_url']
+    pool_size = int(db_params.get('pool_size', 25))
+    max_overflow = int(db_params.get('max_overflow', 5))
+    engine = create_engine(url, pool_size=pool_size, max_overflow=max_overflow)
 
     with Session(engine) as session:
         yield session
