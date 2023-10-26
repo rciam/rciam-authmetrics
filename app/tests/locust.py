05ee05904e745efec0cd0db1e2d661f92c6d8b57
Ioannis Igoumenos <3846025+ioigoume@users.noreply.github.com>
2023-10-26 14:47:15 +0300

    add  locust support (#33)
    * add  locust support
    
    * add requirements dev file
    
    * update readme

---
+++ app/tests/locust.py
@@ -0,0 +1,17 @@
+from locust import HttpUser, task, between
+
+
+class WebsiteUser(HttpUser):
+    # wait_time can be overridden for individual TaskSets
+    wait_time = between(10, 300)
+
+    # Host for locust interface: https://metrics-dev.rciam.grnet.gr
+    @task
+    def metrics_egi_devel_tenenv(self):
+        headers = {
+            'accept': 'application/json',
+            'x-tenant': 'egi',
+            'x-environment': 'devel'
+        }
+
+        self.client.get("/api/v1/tenenv/egi/devel", headers=headers)
